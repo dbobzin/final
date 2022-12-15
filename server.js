2b0c28488957a62b7f3ab241787655b7d06d7162
Dustin <dbobzin@ilstu.edu>
2022-12-15 00:27:38 -0600

    second commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("connected to db & listening on port", process.env.PORT);
     });
   })
